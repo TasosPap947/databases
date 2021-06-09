b919a717dd23de939e891933d247336a5e34c320
nikpag00 <nikospagonas00@gmail.com>
2021-06-09 21:38:01 +0300

    My commit

--- project/data.sql
+++ project/data.sql
@@ -5,24 +5,48 @@ insert into <table>(attr1, attr2, ...) values(value1, value2, ...)
 
 /* customer */
 
+/* kati allo */
+
 /* customer_phone */
+
+/* kati allo */
 
 /* customer_email */
 
+/* kati allo */
+
 /* place */
+
+/* kati allo */
 
 /* service */
 
+/* kati allo */
+
 /* service_with_subscription */
+
+/* kati allo */
 
 /* subscribes */
 
+/* kati allo */
+
 /* service_charge */
+
+/* kati allo */
 
 /* service_without_subscription */
 
+/* kati allo */
+
 /* has_access_to */
+
+/* kati allo */
 
 /* visits */
 
+/* kati allo */
+
 /* offered_in */
+
+/* kati allo */
