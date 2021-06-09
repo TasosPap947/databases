e89b4d28d6c7576caeac9a7f8a8a3abff96063bc
TasosPap947 <anastasispapazaf947@gmail.com>
2021-06-09 21:35:31 +0300

    commit ends

--- project/data.sql
+++ project/data.sql
@@ -5,24 +5,63 @@ insert into <table>(attr1, attr2, ...) values(value1, value2, ...)
 
 /* customer */
 
+
+/*end*/
+
 /* customer_phone */
+
+/*end*/
 
 /* customer_email */
 
+
+/*end*/
+
 /* place */
+
+/*end*/
 
 /* service */
 
+
+
+/*end*/
+
 /* service_with_subscription */
+
+
+
+
+/*end*/
 
 /* subscribes */
 
+
+
+/*end*/
+
 /* service_charge */
+
+/*end*/
 
 /* service_without_subscription */
 
+
+/*end*/
+
 /* has_access_to */
+
+
+/*end*/
 
 /* visits */
 
+
+
+/*end*/
+
 /* offered_in */
+
+
+
+/*end*/
