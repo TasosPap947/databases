1778cb38a427fb95770c35aa31135344608cfa56
nikpag00 <nikospagonas00@gmail.com>
2021-06-09 21:28:04 +0300

    Create data.sql

---
+++ project/data.sql
@@ -0,0 +1,28 @@
+/*
+How to insert:
+insert into <table>(attr1, attr2, ...) values(value1, value2, ...)
+*/
+
+/* customer */
+
+/* customer_phone */
+
+/* customer_email */
+
+/* place */
+
+/* service */
+
+/* service_with_subscription */
+
+/* subscribes */
+
+/* service_charge */
+
+/* service_without_subscription */
+
+/* has_access_to */
+
+/* visits */
+
+/* offered_in */
