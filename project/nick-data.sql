7e8b2bc0da74a385de2e512d0dcc41b339e445a7
nikpag00 <nikospagonas00@gmail.com>
2021-06-10 23:34:28 +0300

    First data

--- project/nick-data.sql
+++ project/nick-data.sql
@@ -1,3 +1,16 @@
+delete from customer;
+delete from customer_email;
+delete from customer_phone;
+delete from has_access_to;
+delete from offered_in;
+delete from place;
+delete from service;
+delete from service_charge;
+delete from service_without_subscription;
+delete from service_with_subscription;
+delete from subscribes;
+delete from visited;
+
 /*
 How to insert:
 insert into <table>(attr1, attr2, ...) values(value1, value2, ...)
@@ -5,65 +18,69 @@ insert into <table>(attr1, attr2, ...) values(value1, value2, ...)
 
 /* customer */
 
-/* nick pagonas */
-/* change */
-
-/*end*/
-
-/* customer_phone */
-
-/*end*/
+insert into customer(date_of_birth, document_authority, document_number, document_type, first_name, last_name, nfc_id)
+values("1970-01-01", "Α.Τ. Αθηνών", "ΑΑ000000", "ID", "Γιώργος", "Παπαδόπουλος", 1);
 
 /* customer_email */
 
+insert into customer_email(customer_id, email)
+values(1, "georgepapad@gmail.com");
 
-/*end*/
+/* customer_phone */
+
+insert into customer_phone(customer_id, phone_number)
+values(1, "0000000000");
 
 /* place */
 
-/*end*/
+insert into place(corridor, floor_number, number_of_beds, place_name, number, place_id)
+values
+("North", 0, 0, "Bar", 1, 1),
+("East", 1, 1, "Room", 101, 2);
 
 /* service */
 
-
-
-/*end*/
+insert into service(service_description, service_id)
+values
+("Drink at bar", 1),
+("Sauna", 2);
 
 /* service_with_subscription */
 
-
-
-
-/*end*/
+insert into service_with_subscription(service_description, service_id)
+values("Sauna", 2); /* Try sauna with id = 3 while sauna has id = 2 at service table */
 
 /* subscribes */
 
-
-
-/*end*/
+insert into subscribes(customer_id, service_id, subscription_date_time)
+values(1, 2, "2000-01-01 00:00:00");
 
 /* service_charge */
 
-/*end*/
+insert into service_charge(charge_amount, charge_date_time, charge_description, customer_id, service_id, charge_id)
+values(100, "2000-01-02 13:40:00", "Strawberry Daquiri", 1, 1, 1);
 
 /* service_without_subscription */
 
-
-/*end*/
+insert into service_without_subscription(service_description, service_id)
+values("Drink at bar", 1);
 
 /* has_access_to */
 
+/* TODO: Add trigger that gives access to those who have subscribed */
 
-/*end*/
+/* NO TRIVIAL ACCESSES!!! */
 
-/* visits */
+insert into has_access_to(customer_id, place_id, start_date_time, end_date_time)
+values(1, 2, "2000-01-01 00:00:00", null);
+
+/* visited */
 
 
-
-/*end*/
+insert into visited(customer_id, entry_date_time, exit_date_time, place_id, visit_id)
+values(1, "2000-01-02 10:00:00", "2000-01-02 11:00:00", 1, 1);
 
 /* offered_in */
 
-
-
-/*end*/
+insert into offered_in(place_id, service_id)
+values(1, 1);
